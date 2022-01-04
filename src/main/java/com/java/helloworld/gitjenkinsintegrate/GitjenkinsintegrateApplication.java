e74a733d1d3aaddcc81f1daa3725415dede2e7aa
Avadhoot <avadhootd99@gmail.com>
2022-01-04 15:48:43 +0530

    Initial Commit

---
+++ src/main/java/com/java/helloworld/gitjenkinsintegrate/GitjenkinsintegrateApplication.java
@@ -0,0 +1,13 @@
+package com.java.helloworld.gitjenkinsintegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitjenkinsintegrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitjenkinsintegrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/helloworld/gitjenkinsintegrate/GitjenkinsintegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.helloworld.gitjenkinsintegrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitjenkinsintegrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
